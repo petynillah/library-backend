09cdc3117b7b43da74a09b2a6c90b57ae119b581
Wambui <petynillah@gmail.com>
2026-08-26 18:14:42 +0300

    changes

--- authentication/authroute.js
+++ authentication/authroute.js
@@ -20,7 +20,9 @@ const {
   getAllStaff, 
   getStaffById, 
   updateStaff, 
-  deleteStaff 
+  deleteStaff,
+  generateSSOTicket, // 👈 ADD THIS
+  exchangeSSOTicket  // 👈 ADD THIS
 } = require('./staffauthcontroller');
 
 
@@ -28,8 +30,8 @@ const {
 const { verifyToken, verify2FA, authorizeRoles } = require('./authmidleware');
 
 
-router.post('/auth/generate-sso-ticket', verifyToken, staffauthcontroller.generateSSOTicket);
-router.post('/auth/exchange-sso-ticket', staffauthcontroller.exchangeSSOTicket);
+router.post('/auth/generate-sso-ticket', verifyToken, generateSSOTicket);
+router.post('/auth/exchange-sso-ticket', exchangeSSOTicket);
 
 // ==========================================
 // 1. PUBLIC ROUTES (No Token Needed)
